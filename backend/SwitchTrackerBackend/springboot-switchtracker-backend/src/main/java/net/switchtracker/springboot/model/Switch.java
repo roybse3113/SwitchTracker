db3c406ad87d8e25ffbb5e1557f0a1c02a12b6f1
roybse3113 <79131282+roybse3113@users.noreply.github.com>
2021-06-18 21:27:05 -0700

    Update Switch.java

--- backend/SwitchTrackerBackend/springboot-switchtracker-backend/src/main/java/net/switchtracker/springboot/model/Switch.java
+++ backend/SwitchTrackerBackend/springboot-switchtracker-backend/src/main/java/net/switchtracker/springboot/model/Switch.java
@@ -1,101 +1,107 @@
 package net.switchtracker.springboot.model;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-
-@Entity
-@Table(name = "switches")
-public class Switch {
-
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private long id;
-	
-	@Column(name="name")
-	private String name;
-	
-	@Column(name="price")
-	private double price;
-	
-	@Column(name="in_stock")
-	private boolean inStock;
-	
-//	private String vendor;
-	
-//	@Autowired
-//	private List<Switch> subSwitches;
-
-	public Switch() {
-		
+public class SwitchParser {
+    private LinkedHashMap<Switch,String> allSwitches;
+//    private HashMap<String,Switch> allSwitches;
+    private LinkedHashMap<String,String> switchLinks;
+    private List<String> allPages = new LinkedList<String>();
+    
+	public SwitchParser() {
+		allPages.add("https://ringerkeys.com/collections/switches/switches?sort_by=best-selling");
+		allPages.add("https://www.primekb.com/collections/switches");
+	    allPages.add("https://novelkeys.xyz/collections/switches?page=1&sort_by=best-selling");
+	    
+//	    Parser p = new Parser("https://novelkeys.xyz/collections/switches/products/the-silk-series");
+//      p.getStockStatus(allSwitches);
+	    switchLinks = new LinkedHashMap<String,String>();
+	    this.allSwitches = new LinkedHashMap<Switch,String>();
+	    
+	    //Get all the links for each switch page
+	    parseAllSwitches();
+	    updateSwitchStatus();
+	    
+//	    for (Map.Entry<Switch, String> entry : allSwitches.entrySet()) {
+//          System.out.println(entry.getKey().getName() + ": " + entry.getKey().getStockStatus());
+//          System.out.println("subSwitches: ");
+//          for (Switch s : entry.getKey().getSubSwitches()) {
+//              System.out.println(s.getName() + ": " + s.getStockStatus());
+//          }
+//          System.out.println("--------------------------");
+//	    }
 	}
 	
-	public Switch(String name) {
-		this.name = name;
+	public void parseAllSwitches() {
+	    Pattern p = Pattern.compile("(.*)\\/collections");
+	    Matcher m;
+	    List<Integer> visitedPages = new ArrayList<Integer>();
+	    LinkedHashMap<Integer,String> links = new LinkedHashMap<Integer,String>();
+	    for (String url: allPages) {
+	        
+	        //Establish the base URL
+	        m = p.matcher(url);
+	        String baseURL = "";
+	        if (m.find()) {
+	            baseURL = m.group(1);
+	        }
+	        
+	      //initalize the map such that it is not empty
+	        links.put(-1, null);
+	        while (!links.isEmpty()) {
+//	          System.out.println(links);
+	            
+	            Parser parse = new Parser(url);
+	            links = parse.getArticles(baseURL, allSwitches,switchLinks,links);
+	            
+//	          System.out.println(visitedPages);
+	            
+	            for (Map.Entry<Integer, String> entry : links.entrySet()) {
+//	              System.out.println("Entry : " + entry.getKey());
+	                if (!visitedPages.contains(entry.getKey())) {
+	                    visitedPages.add(entry.getKey());
+	                    System.out.println(entry.getValue());
+	                    url = baseURL + entry.getValue();
+//	                    System.out.println("URL: " + url);
+	                } 
+	            }
+	        }
+	    }
+	    
+	    
+//	    System.out.println(allSwitches.size());
+//	    for (Map.Entry<Switch, String> entry : allSwitches.entrySet()) {
+//	        System.out.println(entry.getKey().getName() + ": " + entry.getValue());
+//	    }
+//	    System.out.println(allSwitches);
 	}
 	
-	public Switch(String name, double price, boolean inStock) {
-		super();
-		this.name = name;
-		this.price = price;
-		this.inStock = inStock;
+	public void updateSwitchStatus() {
+	    for (Map.Entry<Switch, String> entry : allSwitches.entrySet()) {
+	        Parser parse = new Parser(entry.getValue());
+	        parse.getStockStatus(entry.getKey());
+	    }
 	}
 	
-//	public String getVendor() {
-//		return vendor;
-//	}
-//
-//	public void setVendor(String vendor) {
-//		this.vendor = vendor;
-//	}
-
-//	public List<Switch> getSubSwitches() {
-//		return subSwitches;
-//	}
-//
-//	public void setSubSwitches(List<Switch> subSwitches) {
-//		this.subSwitches = subSwitches;
-//	}
-
-	public long getId() {
-		return id;
-	}
-
-	public void setId(long id) {
-		this.id = id;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public double getPrice() {
-		return price;
-	}
-
-	public void setPrice(double price) {
-		this.price = price;
-	}
-
-	public boolean isInStock() {
-		return inStock;
-	}
-
-	public void setInStock(boolean inStock) {
-		this.inStock = inStock;
+	public LinkedHashMap<String,String> getSwitchLinks() { 
+	    return switchLinks;
 	}
 	
-	
+	public LinkedHashMap<Switch,String> getAllSwitches() {
+		return allSwitches;
+	}
+
 }
