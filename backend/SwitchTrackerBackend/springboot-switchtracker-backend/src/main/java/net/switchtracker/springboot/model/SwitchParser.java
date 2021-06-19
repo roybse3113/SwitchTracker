c6a00c79e51d81604a344393ad00827707865a9e
roybse3113 <79131282+roybse3113@users.noreply.github.com>
2021-06-18 21:27:48 -0700

    Update SwitchParser.java

--- backend/SwitchTrackerBackend/springboot-switchtracker-backend/src/main/java/net/switchtracker/springboot/model/SwitchParser.java
+++ backend/SwitchTrackerBackend/springboot-switchtracker-backend/src/main/java/net/switchtracker/springboot/model/SwitchParser.java
@@ -3,6 +3,8 @@ package net.switchtracker.springboot.model;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.regex.Matcher;
@@ -14,58 +16,72 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 public class SwitchParser {
-    private HashMap<Switch,String> allSwitches;
+    private LinkedHashMap<Switch,String> allSwitches;
 //    private HashMap<String,Switch> allSwitches;
-    private HashMap<String,String> switchLinks;
+    private LinkedHashMap<String,String> switchLinks;
+    private List<String> allPages = new LinkedList<String>();
     
 	public SwitchParser() {
+		allPages.add("https://ringerkeys.com/collections/switches/switches?sort_by=best-selling");
+		allPages.add("https://www.primekb.com/collections/switches");
+	    allPages.add("https://novelkeys.xyz/collections/switches?page=1&sort_by=best-selling");
+	    
 //	    Parser p = new Parser("https://novelkeys.xyz/collections/switches/products/the-silk-series");
 //      p.getStockStatus(allSwitches);
-	    switchLinks = new HashMap<String,String>();
-	    this.allSwitches = new HashMap<Switch,String>();
+	    switchLinks = new LinkedHashMap<String,String>();
+	    this.allSwitches = new LinkedHashMap<Switch,String>();
 	    
 	    //Get all the links for each switch page
 	    parseAllSwitches();
 	    updateSwitchStatus();
 	    
-	    for (Map.Entry<Switch, String> entry : allSwitches.entrySet()) {
-          System.out.println(entry.getKey().getName() + ": " + entry.getKey().isInStock());
+//	    for (Map.Entry<Switch, String> entry : allSwitches.entrySet()) {
+//          System.out.println(entry.getKey().getName() + ": " + entry.getKey().getStockStatus());
 //          System.out.println("subSwitches: ");
 //          for (Switch s : entry.getKey().getSubSwitches()) {
-//              System.out.println(s.getName() + ": " + s.isInStock());
+//              System.out.println(s.getName() + ": " + s.getStockStatus());
 //          }
-          System.out.println("--------------------------");
-	    }
-	}
-	
-	public HashMap<Switch,String> getAllSwitches() {
-		return allSwitches;
+//          System.out.println("--------------------------");
+//	    }
 	}
 	
 	public void parseAllSwitches() {
+	    Pattern p = Pattern.compile("(.*)\\/collections");
+	    Matcher m;
 	    List<Integer> visitedPages = new ArrayList<Integer>();
-	    HashMap<Integer,String> links = new HashMap<Integer,String>();
-	    String url = "https://novelkeys.xyz/collections/switches";
-	    
-	    //initalize the map such that it is not empty
-	    links.put(-1, null);
-	    while (!links.isEmpty()) {
-//	        System.out.println(links);
+	    LinkedHashMap<Integer,String> links = new LinkedHashMap<Integer,String>();
+	    for (String url: allPages) {
 	        
-	        Parser parse = new Parser(url);
-	        links = parse.getArticles(allSwitches,switchLinks,links);
+	        //Establish the base URL
+	        m = p.matcher(url);
+	        String baseURL = "";
+	        if (m.find()) {
+	            baseURL = m.group(1);
+	        }
 	        
-//	        System.out.println(visitedPages);
-	        
-	        for (Map.Entry<Integer, String> entry : links.entrySet()) {
-//	            System.out.println("Entry : " + entry.getKey());
-                if (!visitedPages.contains(entry.getKey())) {
-                    visitedPages.add(entry.getKey());
-                    url = "https://novelkeys.xyz" + entry.getValue();
-//                    System.out.println("URL: " + url);
-                } 
-            }
+	      //initalize the map such that it is not empty
+	        links.put(-1, null);
+	        while (!links.isEmpty()) {
+//	          System.out.println(links);
+	            
+	            Parser parse = new Parser(url);
+	            links = parse.getArticles(baseURL, allSwitches,switchLinks,links);
+	            
+//	          System.out.println(visitedPages);
+	            
+	            for (Map.Entry<Integer, String> entry : links.entrySet()) {
+//	              System.out.println("Entry : " + entry.getKey());
+	                if (!visitedPages.contains(entry.getKey())) {
+	                    visitedPages.add(entry.getKey());
+	                    System.out.println(entry.getValue());
+	                    url = baseURL + entry.getValue();
+//	                    System.out.println("URL: " + url);
+	                } 
+	            }
+	        }
 	    }
+	    
+	    
 //	    System.out.println(allSwitches.size());
 //	    for (Map.Entry<Switch, String> entry : allSwitches.entrySet()) {
 //	        System.out.println(entry.getKey().getName() + ": " + entry.getValue());
@@ -80,8 +96,12 @@ public class SwitchParser {
 	    }
 	}
 	
-	public HashMap<String,String> getSwitchLinks() { 
+	public LinkedHashMap<String,String> getSwitchLinks() { 
 	    return switchLinks;
+	}
+	
+	public LinkedHashMap<Switch,String> getAllSwitches() {
+		return allSwitches;
 	}
 
 }
